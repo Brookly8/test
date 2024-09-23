57bb5d7556dc0a54ebc3eeedbb1469ec848316af
Yurii <jurabartkiv@gmail.com>
2024-09-23 12:30:09 -0500

    ty

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,77 @@
-import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Gallery } from "./components/Gallery/Gallery";
-function App() {
-  const [personList, setPersonList] = useState([]);
+import axios from "axios";
 
+function App() {
+  const [people, setPeople] = useState("");
+  const [person, setPerson] = useState([]);
+
+  const allClickHendler = () => {
+    setPeople("all");
+  };
+
+  const manClickHendler = () => {
+    setPeople("man");
+  };
+
+  const womanClickHendler = () => {
+    setPeople("woman");
+  };
+
+  const GetPeople = async () => {
+    try {
+      const { data } = await axios("https://randomuser.me/api/?results=20");
+      if (people === "man") {
+        const filteredManData = data.results.filter(
+          (item) => item.gender === "male"
+        );
+        setPerson(filteredManData);
+      }
+
+      if (people === "woman") {
+        const filteredWomanData = data.results.filter(
+          (item) => item.gender === "female"
+        );
+        setPerson(filteredWomanData);
+      }
+
+      if (people === "all") {
+        setPerson(data.results);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (people) {
+      GetPeople();
+    }
+  }, [people]);
+  console.log(person);
   return (
     <>
-      <Gallery state={personList} setState={setPersonList}></Gallery>
+      <div className="buttons">
+        <button onClick={allClickHendler}>All</button>
+        <button onClick={manClickHendler}>Man</button>
+        <button onClick={womanClickHendler}>Woman</button>
+      </div>
+      <div className="main">
+        {person.map((p, index) => (
+          <div className="person" key={index}>
+            <img src={p.picture.large} alt="" />
+            <p className="name">
+              {p.name.first} {p.name.last}
+            </p>
+            <p>
+              {p.location.country}, {p.location.city}
+            </p>
+            <p>age:{p.dob.age}</p>
+            <p>cell: {p.cell}</p>
+            <p>Email: {p.email}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
